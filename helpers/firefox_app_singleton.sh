780143bcc4153f4518f0eeb6e39620951aa78dc1
brotifypacha <brotifypacha@gmail.com>
2022-10-31 23:28:34 +0300

    Add support for multiple tabs in firefox_app_singleton

--- helpers/firefox_app_singleton.sh
+++ helpers/firefox_app_singleton.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 if [ -z "$(firefox -v)" ] ; then notify-send "Unable to start $1 firefox is required"; fi
-xdo activate -N "firefox_app_$1" || firefox --kiosk --class "firefox_app_$1" $2 &
+class="firefox_app_$1"
+shift
+xdo activate -N "$class" || firefox --class "$class" $@ &
