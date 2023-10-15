da0b51dda6c099c47ff460d20ecde8118e92bf62
brotifypacha <brotifypacha@gmail.com>
2023-10-15 20:58:52 +0300

    add spotify

--- helpers/firefox_app_singleton.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-if [ -z "$(firefox -v)" ] ; then notify-send "Unable to start $1 firefox is required"; fi
-class="firefox_app_$1"
-shift
-xdo activate -N "$class" || firefox --class "$class" $@ &
